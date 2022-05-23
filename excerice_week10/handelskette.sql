1f1070ac441eea7f86436a117c486291bfe4b7e7
ubuntuuser <asdf>
2022-05-23 17:31:49 +0200

    ex10

--- excerice_week10/handelskette.sql
+++ excerice_week10/handelskette.sql
@@ -1,72 +1,5 @@
-## Aufgabe 1 
-![./aufgabe_eins.jpeg](./aufgabe_eins.jpeg)
-
-# Aufgabe 2 (ER-Diagram fur Kinokette)
-![./aufgabe_zwei.jpeg](./aufgabe_zwei.jpeg)
-## Schema aus ERD 
-```sql
-
-CREATE TABLE kino (
-	id INTEGER PRIMARY KEY,
-	name TEXT NOT NULL,
-	address TEXT NOT NULL,
-);
 
 
-CREATE TABLE saal (
-	id INTEGER PRIMARY KEY,
-	name TEXT NOT NULL,
-    kino_id INTEGER NOT NULL
-    FOREIGN KEY (kino_id)
-       REFERENCES kino (kino_id)
-);
-
-
-CREATE TABLE film (
-	id INTEGER PRIMARY KEY,
-	name TEXT NOT NULL,
- 	length_min REAL NOT NULL,
-);
-
-CREATE TABLE vorfuehrung (
-	id INTEGER PRIMARY KEY,
-    film_id INTEGER NOT NULL
-    FOREIGN KEY (film_id)
-       REFERENCES film (film_id)
-    sprache_id INTEGER NOT NULL
-    FOREIGN KEY (sprache_id)
-       REFERENCES sprache (sprache_id)
-    saal_id INTEGER NOT NULL
-    FOREIGN KEY (saal_id)
-       REFERENCES saal (saal_id)
-    start_ts_sec INTEGER NOT NULL
-);
-
-CREATE TABLE ticket (
-	id INTEGER PRIMARY KEY,
-	price REAL NOT NULL,
-    vorfuehrung_id INTEGER NOT NULL
-    FOREIGN KEY (vorfuehrung_id)
-       REFERENCES vorfuehrung (vorfuehrung_id)
-);
-
-CREATE TABLE sprache (
-	id INTEGER PRIMARY KEY,
-	iso_code TEXT NOT NULL,
-);
-
-CREATE TABLE person (
-	id INTEGER PRIMARY KEY,
-	name TEXT NOT NULL,
-);
-``` 
-
-
-# Aufgabe 3 
-![./aufgabe_trei.jpeg](./aufgabe_trei.jpeg)
-
-## 2. DB Schema 
-```sql 
 CREATE TABLE produkt (
 	id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
@@ -128,25 +61,17 @@ CREATE TABLE filiale (
 	name TEXT NOT NULL,
    adresse TEXT NOT NULL
 );
-```
-## 3. sql queries
 
-### a. 
-```sql
+
 select * from lieferung where date > '2005-01-12'
-```
-### b. 
-```sql 
+
 select * from lieferung 
    join holding on lieferung.holding_id = holding.id
    where holding.name = "ABC Holding"
-```
 
-### c. 
-```sql 
+
 select ALL from lieferung 
    join holding on lieferung.holding_id = holding.id
    where holding.name = "ABC Holding"
    UNION ALL 
    WHERE TRUE; 
-```
